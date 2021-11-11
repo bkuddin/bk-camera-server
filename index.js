d7066d9b47a63a9dd7d4fc327d875e5cdeaf963d
BK Uddin <1bkhossain@gmail.com>
2021-11-11 23:30:19 +0600

    Updated Admin

--- index.js
+++ index.js
@@ -26,6 +26,8 @@ async function run() {
     const database = client.db("bkCameras");
     const camerasCollection = database.collection("cameras");
     const ordersCollection = database.collection("orders");
+    const reviewsCollection = database.collection("reviews");
+    const usersCollection = database.collection("users");
 
     // API POST
     app.post("/addCameras", async (req, res) => {
@@ -65,6 +67,87 @@ async function run() {
       const result = await ordersCollection.insertOne(order);
       console.log(result);
       res.json(result);
+    });
+
+    // API GET for My Order Page, through specific email
+    app.get("/orders/:email", async (req, res) => {
+      console.log(req.params.email);
+
+      const result = await ordersCollection
+        .find({ email: req.params.email })
+        .toArray();
+      // console.log(result);
+      res.json(result);
+    });
+
+    // Delete API for My Order Page,
+    app.delete("/orders/:id", async (req, res) => {
+      console.log(req.params.id);
+      const result = await ordersCollection.deleteOne({
+        _id: ObjectId(req.params.id),
+      });
+      // console.log(result);
+      res.json(result);
+    });
+
+    // API POST for Make Review
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewsCollection.insertOne(review);
+      console.log(result);
+      res.json(result);
+    });
+
+    // API GET For Reviews Page
+    app.get("/reviews", async (req, res) => {
+      const cursor = reviewsCollection.find({});
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
+    // API POST for Save User
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      console.log(result);
+      res.json(result);
+    });
+    // API PUT for Update User (Upsert Case especially for Google sign-in)
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      console.log(result);
+      res.json(result);
+    });
+
+    // API PUT Make Admin
+    app.put("/users/admin", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const updateDoc = { $set: { role: "admin" } };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      console.log(result);
+      res.json(result);
+    });
+
+    // API GET For users
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+
+      res.send({ admin: isAdmin });
     });
   } finally {
     // await client.close();
